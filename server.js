91ff61865fe455ead65a5bdb6f8684c96d368da1
antriksh21 <41363401+antriksh21@users.noreply.github.com>
2020-10-08 20:15:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+const db = require('./config/keys').MongoURI;
+//connect mongodb
+mongoose.connect(db, { useNewUrlParser: true})
+.then(() => console.log('MongoDB conntected'))
+.catch(err => console.log(err));
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
